744f0d3e511ab4260d699964ec8fb538133369e5
dayun <linda7567@naver.com>
2019-07-23 18:49:20 +0900

    190723

---
+++ src/main/java/kr/ac/ju/dao/EmployeeDao.java
@@ -0,0 +1,8 @@
+package kr.ac.ju.dao;
+
+import kr.ac.ju.vo.Employee;
+
+public interface EmployeeDao {
+
+	Employee getEmployeeByNo(int employeeNo);
+}
---
+++ src/main/java/kr/ac/ju/dao/ProfessorDao.java
@@ -0,0 +1,8 @@
+package kr.ac.ju.dao;
+
+import kr.ac.ju.vo.Professor;
+
+public interface ProfessorDao {
+
+	Professor getProfessorByNo(int professorNo);
+}
