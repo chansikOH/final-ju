288a3b3c34eef42f98a6fecb5eb6608cd2fbedd4
yang <hlyang26@gmail.com>
2019-07-23 18:10:13 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/kr/ac/ju/vo/Department.java
    #	src/main/java/kr/ac/ju/vo/Employee.java
    #	src/main/java/kr/ac/ju/vo/Position.java

--- src/main/java/kr/ac/ju/vo/Position.java
+++ src/main/java/kr/ac/ju/vo/Position.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package kr.ac.ju.vo;
 
 import java.util.Date;
@@ -46,52 +45,3 @@ public class Position {
 	
 	
 }
-=======
-package kr.ac.ju.vo;
-
-import java.util.Date;
-
-public class Position {
-
-	private String id;
-	private String name;
-	private Integer level;
-	private Date creatDate;
-	
-	public Position () {}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getLevel() {
-		return level;
-	}
-
-	public void setLevel(Integer level) {
-		this.level = level;
-	}
-
-	public Date getCreatDate() {
-		return creatDate;
-	}
-
-	public void setCreatDate(Date creatDate) {
-		this.creatDate = creatDate;
-	}
-	
-	
-}
->>>>>>> origin/master
